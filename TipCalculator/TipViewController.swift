bf1b48d1217e4f008a7a748b68f3f17a09674bd7
Varun Kochar <varunkochar08@gmail.com>
2017-08-06 23:13:13 -0700

    Basic functionality

--- TipCalculator/TipViewController.swift
+++ TipCalculator/TipViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class TipViewController: UIViewController {
 
     @IBOutlet weak var billAmountLabel: UITextField!
+    
     @IBOutlet weak var tipPercentageControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
     
     @IBOutlet weak var shareControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var numberOfPeopleLabel: UILabel!
     @IBOutlet weak var personImage: UIImageView!
     
+    let tipArray = [0.15, 0.18, 0.2]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,50 @@ class TipViewController: UIViewController {
 
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
+    }
+    
+    private func calculateTip() {
+        
+        let billAmount = Double(billAmountLabel.text!) ?? 0
+        
+        if (billAmount == 0) {
+            tipLabel.text = "$"
+            totalLabel.text = "$"
+            return
+        }
+        
+        let numberOfPeopleToShareBetween = Double(numberOfPeopleLabel.text!) ?? 1
+        let myBillAmount = billAmount / numberOfPeopleToShareBetween
+        
+        let tipAmount = myBillAmount * tipArray[tipPercentageControl.selectedSegmentIndex]
+        let totalAmount = myBillAmount + tipAmount
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+    }
+    
+    @IBAction func didUpdateBillAmount(_ sender: Any) {
+        calculateTip()
+    }
+    
+    @IBAction func didSelectTipPercentage(_ sender: Any) {
+        calculateTip()
+    }
+    
+    @IBAction func didChangeShareAmongNumber(_ sender: Any) {
+        var currentShareAmong = Int(numberOfPeopleLabel.text!) ?? 1
+        
+        if (shareControl.selectedSegmentIndex == 1) {
+            currentShareAmong += 1
+        } else if (shareControl.selectedSegmentIndex == 0 && currentShareAmong > 1) {
+            currentShareAmong -= 1
+        }
+        
+        shareControl.selectedSegmentIndex = -1
+        
+        numberOfPeopleLabel.text = "\(currentShareAmong)"
+        
+        calculateTip()
     }
 
 }
